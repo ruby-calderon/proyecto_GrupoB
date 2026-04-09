f9bbc5f208f33830567ce52a875636a6281b0f2f
Ruby Fenco <rfencocalderon@gmail.com>
2026-04-09 09:29:50 -0500

    limpieza de advertencias y se agregó variables globales

---
+++ Proyecto_Final/src/Proyecto/ConfigurarObsequios.java
@@ -0,0 +1,85 @@
+package Proyecto;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ConfigurarObsequios extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField txtUnidad;
+	private JTextField txtDosAcinco;
+	private JTextField txtSeisAmas;
+	private JLabel lblUnidad;
+	private JLabel lblDosAcinco;
+	private JLabel lblSeisAmas;
+	private JButton btnAceptar;
+	private JButton btnCancelar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ConfigurarObsequios dialog = new ConfigurarObsequios();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ConfigurarObsequios() {
+		setTitle("Configurar Obsequios");
+		setBounds(100, 100, 450, 165);
+		getContentPane().setLayout(null);
+		{
+			lblUnidad = new JLabel("1 Unidad");
+			lblUnidad.setBounds(20, 31, 102, 13);
+			getContentPane().add(lblUnidad);
+		}
+		{
+			lblDosAcinco = new JLabel("2 a 5 unidades");
+			lblDosAcinco.setBounds(20, 54, 102, 13);
+			getContentPane().add(lblDosAcinco);
+		}
+		{
+			lblSeisAmas = new JLabel("6 a m\u00E1s unidades");
+			lblSeisAmas.setBounds(20, 77, 102, 13);
+			getContentPane().add(lblSeisAmas);
+		}
+		{
+			txtUnidad = new JTextField();
+			txtUnidad.setBounds(147, 28, 96, 19);
+			getContentPane().add(txtUnidad);
+			txtUnidad.setColumns(10);
+		}
+		{
+			txtDosAcinco = new JTextField();
+			txtDosAcinco.setBounds(147, 51, 96, 19);
+			getContentPane().add(txtDosAcinco);
+			txtDosAcinco.setColumns(10);
+		}
+		{
+			txtSeisAmas = new JTextField();
+			txtSeisAmas.setBounds(147, 74, 96, 19);
+			getContentPane().add(txtSeisAmas);
+			txtSeisAmas.setColumns(10);
+		}
+		{
+			btnAceptar = new JButton("Aceptar");
+			btnAceptar.setBounds(341, 27, 85, 21);
+			getContentPane().add(btnAceptar);
+		}
+		{
+			btnCancelar = new JButton("Cancelar");
+			btnCancelar.setBounds(341, 50, 85, 21);
+			getContentPane().add(btnCancelar);
+		}
+	}
+
+}
---
+++ Proyecto_Final/src/Proyecto/ConsultarAuto.java
@@ -0,0 +1,153 @@
+package Proyecto;
+
+import java.awt.BorderLayout;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+public class ConsultarAuto extends JDialog implements ActionListener {
+
+	private static final long serialVersionUID = 32705407359759178L;
+	private final JPanel contentPanel = new JPanel();
+	private JLabel lblPrecio;
+	private JTextField txtPrecio;
+	private JLabel lblMarca;
+	private JTextField txtModelo;
+	private JButton btnCerrar;
+	private JLabel lblModelo;
+	private JComboBox<String> cboMarca;
+	private JLabel lblTorque;
+	private JTextField txtTorque;
+	private JLabel lblStock;
+	private JTextField txtStock;
+	private JLabel lblGarantia;
+	private JTextField txtGarantia;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		try {
+			ConsultarAuto dialog = new ConsultarAuto();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ConsultarAuto() {
+		setTitle("Consultar Autos");
+		setResizable(false);
+		setBounds(100, 100, 500, 250);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+
+		lblModelo = new JLabel("Modelo");
+		lblModelo.setBounds(21, 48, 46, 14);
+		contentPanel.add(lblModelo);
+
+		lblMarca = new JLabel("Marca");
+		lblMarca.setBounds(21, 17, 46, 14);
+		contentPanel.add(lblMarca);
+
+		lblPrecio = new JLabel("Precio ($.)");
+		lblPrecio.setBounds(21, 79, 73, 14);
+		contentPanel.add(lblPrecio);
+
+		cboMarca = new JComboBox<String>();
+		cboMarca.addActionListener(this);
+		cboMarca.setModel(new DefaultComboBoxModel<>(new String[] { "TOYOTA", "HONDA", "HYUNDAI", "KIA", "MAZDA" }));
+		cboMarca.setBounds(138, 14, 157, 20);
+		contentPanel.add(cboMarca);
+
+		txtModelo = new JTextField();
+		txtModelo.setEditable(false);
+		txtModelo.setBackground(SystemColor.control);
+		txtModelo.setBounds(138, 45, 157, 20);
+		contentPanel.add(txtModelo);
+		txtModelo.setColumns(10);
+
+		txtPrecio = new JTextField();
+		txtPrecio.setBackground(SystemColor.control);
+		txtPrecio.setEditable(false);
+		txtPrecio.setBounds(139, 76, 156, 20);
+		contentPanel.add(txtPrecio);
+		txtPrecio.setColumns(10);
+
+		btnCerrar = new JButton("Cerrar");
+		btnCerrar.addActionListener(this);
+		btnCerrar.setBounds(358, 14, 89, 23);
+		contentPanel.add(btnCerrar);
+
+		lblTorque = new JLabel("Torque (Nm)");
+		lblTorque.setBounds(21, 110, 73, 14);
+		contentPanel.add(lblTorque);
+
+		txtTorque = new JTextField();
+		txtTorque.setEditable(false);
+		txtTorque.setColumns(10);
+		txtTorque.setBackground(SystemColor.control);
+		txtTorque.setBounds(139, 107, 156, 20);
+		contentPanel.add(txtTorque);
+
+		lblStock = new JLabel("Stock");
+		lblStock.setBounds(21, 141, 46, 14);
+		contentPanel.add(lblStock);
+
+		txtStock = new JTextField();
+		txtStock.setEditable(false);
+		txtStock.setColumns(10);
+		txtStock.setBackground(SystemColor.control);
+		txtStock.setBounds(139, 138, 156, 20);
+		contentPanel.add(txtStock);
+
+		lblGarantia = new JLabel("Garantia (a\u00F1os)");
+		lblGarantia.setBounds(21, 172, 94, 14);
+		contentPanel.add(lblGarantia);
+
+		txtGarantia = new JTextField();
+		txtGarantia.setEditable(false);
+		txtGarantia.setColumns(10);
+		txtGarantia.setBackground(SystemColor.control);
+		txtGarantia.setBounds(139, 169, 156, 20);
+		contentPanel.add(txtGarantia);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == cboMarca) {
+			actionPerformedCboCodigo(e);
+		}
+		if (e.getSource() == btnCerrar) {
+			actionPerformedBtnCerrar(e);
+		}
+	}
+
+	protected void actionPerformedBtnCerrar(ActionEvent e) {
+		dispose();
+	}
+
+	protected void actionPerformedCboCodigo(ActionEvent e) {
+
+	}
+}
